990b789fea4bf9b7b4a37af615797af305626bf4
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-03 14:12:28 -0700

    Main now runs commands. Registered 'login' command.

--- main.go
+++ main.go
@@ -3,37 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/sbrown3212/gator/internal/config"
 )
-
-func main() {
-	cfg, err := config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("config: %+v\n", cfg)
-
-	err = cfg.SetUser("stephen")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
-	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-
-	fmt.Printf("config again: %+v\n", cfg)
-}
 
 type state struct {
 	cfg *config.Config
 }
 
 type command struct {
-	name string
-	args []string
+	Name string
+	Args []string
 }
 
 type commands struct {
@@ -42,7 +23,7 @@ type commands struct {
 
 // Run the given command with the current state, if it exists.
 func (c *commands) run(s *state, cmd command) error {
-	err := c.handlers[cmd.name](s, cmd)
+	err := c.handlers[cmd.Name](s, cmd)
 	if err != nil {
 		return err
 	}
@@ -55,11 +36,11 @@ func (c *commands) register(name string, f func(*state, command) error) {
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
+	if len(cmd.Args) == 0 {
 		return fmt.Errorf("please provide a username with the login command")
 	}
 
-	username := cmd.args[0]
+	username := cmd.Args[0]
 
 	err := s.cfg.SetUser(username)
 	if err != nil {
@@ -68,4 +49,34 @@ func handlerLogin(s *state, cmd command) error {
 
 	fmt.Printf("User set to %s\n", username)
 	return nil
+}
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("config: %+v\n", cfg)
+
+	programState := state{cfg: &cfg}
+
+	handlers := make(map[string]func(*state, command) error)
+	cmds := commands{handlers: handlers}
+
+	cmds.register("login", handlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("please provide at least two arguments")
+	}
+
+	cmdName := args[1]
+	cmdArgs := args[2:]
+
+	cmd := command{Name: cmdName, Args: cmdArgs}
+
+	err = cmds.run(&programState, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
