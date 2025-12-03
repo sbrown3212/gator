1c440dd01934a480b98fc1d593c67d522e180c5e
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-03 12:26:16 -0700

    Initial commands 'run' and 'register' methods. Initial state struct. Initial 'handlerLogin' function.

--- main.go
+++ main.go
@@ -26,3 +26,46 @@ func main() {
 
 	fmt.Printf("config again: %+v\n", cfg)
 }
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+// Run the given command with the current state, if it exists.
+func (c *commands) run(s *state, cmd command) error {
+	err := c.handlers[cmd.name](s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Add a command to the commands struct.
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("please provide a username with the login command")
+	}
+
+	username := cmd.args[0]
+
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return fmt.Errorf("error setting username: %v", err)
+	}
+
+	fmt.Printf("User set to %s\n", username)
+	return nil
+}
