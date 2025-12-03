f1cf98e489c3026b83004201bd6ce9726824ac36
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-03 10:40:57 -0700

    Created 'config' package and 'Read' func.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,42 @@
 // Package config reads and writes config to ~/.gatorconfig.json
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	configFile = ".gatorconfig.json"
+)
+
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to get user's home dir: %s", err)
+	}
+
+	f, err := os.Open(homePath + "/" + configFile)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to open config file: %s", err)
+	}
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to read config file: %s", err)
+	}
+
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return Config{}, fmt.Errorf("failed to unmarshal json: %s", err)
+	}
+
+	return cfg, nil
 }
--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"log"
+
+	"github.com/sbrown3212/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal("Unable to read config:", err)
+	}
+
+	fmt.Printf("config: %+v\n", cfg)
+}
