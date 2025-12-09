f9e15aa48b17743c8a69f5311468a290e920a663
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-12-09 10:27:01 -0700

    Improved output styling for 'addfeed' command.

--- handler_add_feed.go
+++ handler_add_feed.go
@@ -37,7 +37,17 @@ func handlerAddFeed(s *state, cmd command) error {
 		return fmt.Errorf("error creating feed db entry: %s", err)
 	}
 
-	fmt.Printf("%v\n", feed)
+	fmt.Println("Feed created successfully!")
+	printFeed(feed)
 
 	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf(" * ID:      %s\n", feed.ID)
+	fmt.Printf(" * Created: %v\n", feed.CreatedAt)
+	fmt.Printf(" * Updated: %v\n", feed.UpdatedAt)
+	fmt.Printf(" * Name:    %s\n", feed.Name)
+	fmt.Printf(" * URL:     %s\n", feed.Url)
+	fmt.Printf(" * UserID:  %s\n", feed.UserID)
 }
